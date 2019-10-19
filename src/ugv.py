63e045d3613d62c3ab1606923a86bfb1ac3f9605
jeremyszeto <jeremyszeto@users.noreply.github.com>
2019-10-19 12:08:40 -0700

    setup message link with gcs

--- src/ugv.py
+++ src/ugv.py
@@ -1,4 +1,6 @@
 """Autonomous UGV"""
+import json
+from coms import Coms
 
 class UGV:
     def __init__(self):
@@ -8,3 +10,13 @@ class UGV:
     def setup_vehcicle(self):
         #TODO
         return None
+
+    def coms_callback(self, message, _):
+        '''callback for radio messages'''
+        data = json.loads(message.data)
+        print(data['type'])
+
+    def setup_coms(self):
+        '''sets up communication radios'''
+        print('Initializing Coms')
+        self.coms = Coms(self.configs, self.coms_callback)
